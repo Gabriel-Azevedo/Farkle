8a197f124ab828d96a5af8d48086019f7d6c8928
Gabriel_Azevedo <gabriel.azevedo95@icloud.com>
2015-01-14 13:55:54 -0800

    First Commit

---
+++ DieLabel.h
@@ -0,0 +1,13 @@
+//
+//  DieLabel.h
+//  Farkle
+//
+//  Created by Gabriel Borri de Azevedo on 1/14/15.
+//  Copyright (c) 2015 Gabriel Enterprises. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DieLabel : UILabel
+
+@end
---
+++ DieLabel.m
@@ -0,0 +1,26 @@
+//
+//  DieLabel.m
+//  Farkle
+//
+//  Created by Gabriel Borri de Azevedo on 1/14/15.
+//  Copyright (c) 2015 Gabriel Enterprises. All rights reserved.
+//
+
+#import "DieLabel.h"
+
+@implementation DieLabel
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+-(IBAction)onTapped:(UITapGestureRecognizer *)sender
+{
+
+}
+
+@end
